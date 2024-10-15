2c27fc2f4c47acab436e9f97cf3f7deffb4ab3aa
doasu <doasu@pm.me>
2024-10-16 01:02:01 +0200

    test(TrackingSet): add BeforeEach hook

--- src/test/TrackingSetSpec.java
+++ src/test/TrackingSetSpec.java
@@ -1,10 +1,27 @@
 package test;
 
 import main.TrackingSet;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
 
-public class TrackingSetSpec
+class TrackingSetSpec
 {
+
+// A new instance of a TrackingSet will
+// be placed here before each test.
+private TrackingSet<Object> trackingSet;
+
+@BeforeEach
+void setUpTestInstance()
+{
+        trackingSet = new TrackingSet<>();
+        trackingSet.add("Ala ma kota");
+        trackingSet.add("Hello, world!");
+        trackingSet.add(2137);
+        trackingSet.add(13.37D);
+        trackingSet.add(4.20F);
+}
+
 }
