43994f8f3d1a4e236f2224366bfff87c9c81a1d3
doasu <doasu@pm.me>
2024-10-15 16:32:10 +0200

    style(global): change code style

--- src/main/BasicUtils.java
+++ src/main/BasicUtils.java
@@ -1,20 +1,24 @@
 package main;
 
-public class BasicUtils {
-    public static String reversed(String str) {
+public class BasicUtils
+{
+public static String reversed(String str)
+{
         return new StringBuilder(str).reverse().toString();
-    }
+}
 
-    public static int vowelCount(String str) {
+public static int vowelCount(String str)
+{
         long count = str.toLowerCase().chars()
                 .mapToObj(i -> (char) i)
                 .filter(BasicUtils::isVowel)
                 .count();
 
         return (int) count;
-    }
+}
 
-    public static boolean isVowel(char c) {
+public static boolean isVowel(char c)
+{
         return "aeio".contains(String.valueOf(c));
-    }
+}
 }
--- src/main/TrackingSet.java
+++ src/main/TrackingSet.java
@@ -3,49 +3,64 @@ package main;
 import java.util.HashSet;
 import java.util.Objects;
 
-public class TrackingSet<T> {
-    private final HashSet<T> innerSet = new HashSet<>();
+public class TrackingSet<T>
+{
+private final HashSet<T> innerSet = new HashSet<>();
 
-    private int additionsCount = 0;
-    private int removalsCount = 0;
+private int additionsCount = 0;
+private int removalsCount  = 0;
 
-    public void add(T item) {
+public void add(T item)
+{
         innerSet.add(item);
         additionsCount += 1;
-    }
+}
 
-    public void remove(T item) {
+public void remove(T item)
+{
         innerSet.remove(item);
         removalsCount += 1;
-    }
+}
 
-    public int getOperationsCount() {
+public int getOperationsCount()
+{
         return getAdditionsCount() + getRemovalsCount();
-    }
+}
 
-    public int getAdditionsCount() {
+public int getAdditionsCount()
+{
         return additionsCount;
-    }
+}
 
-    public int getRemovalsCount() {
+public int getRemovalsCount()
+{
         return removalsCount;
-    }
+}
 
-    @Override
-    public String toString() {
+@Override
+public String toString()
+{
         return innerSet.toString();
-    }
+}
 
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
+@Override
+public boolean equals(Object o)
+{
+        if (this == o)
+        {
+                return true;
+        }
+        if (o == null || getClass() != o.getClass())
+        {
+                return false;
+        }
         TrackingSet<?> that = (TrackingSet<?>) o;
         return Objects.equals(innerSet, that.innerSet);
-    }
+}
 
-    @Override
-    public int hashCode() {
+@Override
+public int hashCode()
+{
         return Objects.hashCode(innerSet);
-    }
+}
 }
