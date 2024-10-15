4a1f54a53be6f6931a2a3590bbf9666edf12c238
doasu <doasu@pm.me>
2024-10-15 17:03:15 +0200

    test(BasicUtils): add spec for vowelCount

--- src/test/BasicUtilsSpec.java
+++ src/test/BasicUtilsSpec.java
@@ -25,4 +25,56 @@ public void reversingNullReturnsNull()
         Assertions.assertNull(BasicUtils.reversed(null));
 }
 
+@Test
+public void countingVowelsInArbitraryStringProducesCorrectResult()
+{
+        Assertions.assertEquals(
+                5, BasicUtils.vowelCount("Ala ma kota")
+        );
+
+        Assertions.assertEquals(
+                19, BasicUtils.vowelCount(
+                        "Lorem ipsum dolor sit amet, "
+                        + "consectetur adipiscing elit.")
+        );
+}
+
+@Test
+public void countingVowelsInEmptyStringReturnsZero()
+{
+        Assertions.assertEquals(0, BasicUtils.vowelCount(""));
+}
+
+@Test
+public void countingVowelsInVowelStringProducesCorrectResult()
+{
+        Assertions.assertEquals(
+                7, BasicUtils.vowelCount("aoieoyu")
+        );
+
+        Assertions.assertEquals(
+                6, BasicUtils.vowelCount("yuoiea")
+        );
+}
+
+@Test
+public void countingVowelsInConsonantStringReturnsZero()
+{
+        Assertions.assertEquals(
+                0, BasicUtils.vowelCount("qlmlqwnwbvmjbnfbm")
+        );
+
+        Assertions.assertEquals(
+                0, BasicUtils.vowelCount("lslsnnzxcvvbbt")
+        );
+}
+
+@Test
+public void countingVowelsInNullReturnsThrowsException()
+{
+        Assertions.assertThrows(
+                NullPointerException.class, () -> BasicUtils.vowelCount(null)
+        );
+}
+
 }
