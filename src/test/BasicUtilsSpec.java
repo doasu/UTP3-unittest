2089d212c7d902e4b280173ab197e6374838357c
doasu <doasu@pm.me>
2024-10-15 16:36:29 +0200

    test(BasicUtils): add reversingArbitraryStringProducesCorrectResult

--- src/test/BasicUtilsSpec.java
+++ src/test/BasicUtilsSpec.java
@@ -7,6 +7,14 @@ public class BasicUtilsSpec
 
 @Test
 public void reversingArbitraryStringProducesCorrectResult()
-{}
+{
+        Assertions.assertEquals(
+                "txet modnar", BasicUtils.reversed("random text")
+        );
+
+        Assertions.assertEquals(
+                "bd#sa6f3basż;", BasicUtils.reversed(";żsab3f6as#db")
+        );
+}
 
 }
